13347e84818f974d9b798cfd48bdaeeb636f8b5c
Miguel Arenas <migue.arenas91@gmail.com>
2024-04-12 23:42:14 -0500

    npm init root folder

--- api/src/config/auth.config.js
+++ api/src/config/auth.config.js
@@ -2,5 +2,5 @@
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 //*NodeMailer
-export const AdminEmail = process.env.ADMIN_EMAIL
-export const NodeMailerPass = process.env.NODE_PASS;
+export const AdminEmail = process.env.NODEMAILER_USER;
+export const NodeMailerPass = process.env.NODEMAILER_PASS;
