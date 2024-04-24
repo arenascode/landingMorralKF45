d966cf939cb42088f3f89d2925121f890052d430
Miguel Arenas <migue.arenas91@gmail.com>
2024-04-24 10:17:47 -0500

    cors policy remove to be managed by nginx

--- api/src/app/app.js
+++ api/src/app/app.js
@@ -7,20 +7,20 @@ import db from "../connect.js";
 
 export const app = express()
 app.use(cookieParser(secretCookie));
-app.use(
-  cors({
-    origin: [
-      "http://127.0.0.1:5173",
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "https://morralkf45.shop",
-      "https://www.morralkf45.shop",
-      "https://77.37.63.218"
-    ],
-    methods: "GET,HEAD,PUT,POST,DELETE",
-    // credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://127.0.0.1:5173",
+//       "http://localhost:3000",
+//       "http://localhost:5173",
+//       "https://morralkf45.shop",
+//       "https://www.morralkf45.shop",
+//       "https://77.37.63.218"
+//     ],
+//     methods: "GET,HEAD,PUT,POST,DELETE",
+//     // credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use('/static', express.static("public"))
 app.use(express.urlencoded({ extended: true })); // This line allows that the server can interpret the best wall all of data that travel from URL and map it correctly in the req.query
